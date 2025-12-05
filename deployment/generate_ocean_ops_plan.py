ae577fb0e2836441740e17873c8fceb0db1b1ff2
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2025-12-05 10:16:19 -0400

    updated deployment pland and ocean-ops file

--- deployment/generate_ocean_ops_plan.py
+++ deployment/generate_ocean_ops_plan.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 df = pd.read_csv('canada_deployments.csv')
 serial_map = pd.read_csv('serial_info.csv').set_index('serial_prefix')
+program_map = pd.read_csv('program_info.csv').set_index('institute')
 
 # NKE floats only, write APEX interpreter later
 df = df.loc[df.STATUS == 'CONFIRMED']
 df['SERIAL NUMBER'] = [sn if len(sn.split('-')) > 1 else f'TWR-{sn}' for sn in df['SERIAL NUMBER']]
 df = df.drop(['STATUS', 'MODEL', 'MODEL_DETAIL', 'BASIN'], axis=1)
 df['MODEL'] = [serial_map.loc[s.split('-')[0], 'model'] for s in df['SERIAL NUMBER']]
+df['PROGRAM'] = [program_map.loc[inst, 'program'] for inst in df['INSTITUTE']]
 df['SENSOR_MODEL'] = [serial_map.loc[s.split('-')[0], 'sensor_model'] for s in df['SERIAL NUMBER']]
 df['SERIAL NUMBER'] = [sn.replace('TWR-', '') for sn in df['SERIAL NUMBER']]
 
